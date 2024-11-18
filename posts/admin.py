b0a2cff1c000d2122d76ca2924f1f326dcc494f6
ayrikmoradi <mahdimo12345y@gamil.com>
2024-11-18 22:36:03 +0330

    Admin panel personalization

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from .models import Article
 
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields': ('titel', 'author')
+        }),
+        ('Important dates', {
+            'fields': ('created', 'edited')
+        }),
+        ('Content', {
+            'fields': ('content',)
+        })
+    ]
+    readonly_fields = ('created', 'edited')
+    list_display = ('titel', 'author', 'status')
+    list_filter = ('status',)
+    search_fields = ('titel', 'author__username')
